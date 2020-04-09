ebdf75b64171a42963c30794bb49ce3c1d8911dc
Hamaddogar <hamadali77115033@gmail.com>
2020-04-09 07:53:10 +0500

    Assignmrt 3

---
+++ Assigmnet3/js/Script.js
@@ -0,0 +1,38 @@
+var canspeak = true;
+var age = 34;
+learnFunction();
+function learnFunction() {
+
+    var checkobject = {
+        name: "Hamad",
+        fatherName: "Muhammad Hamad",
+        newfunctionl: function () {
+            return "thus s", this
+
+        }
+
+    }
+    if (canspeak && age > 45) {
+        console.log("You are good Human")
+
+
+    }
+
+    else if (canspeak || age > 46) {
+
+        console.log("this is function", checkobject.newfunctionl())
+        console.log("One Condition is true")
+
+    }
+    else {
+
+        console.log("You are   not good Human")
+
+
+
+    }
+
+
+
+}
+
