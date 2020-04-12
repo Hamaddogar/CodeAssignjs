6caaca0c85c17f7e454cf0bc93e92e84fcaac6f7
Hamaddogar <hamadali77115033@gmail.com>
2020-04-12 09:01:53 +0500

    strick non strick mode

--- Assigmnet3/js/Script.js
+++ Assigmnet3/js/Script.js
@@ -1,5 +1,7 @@
 var canspeak = true;
 var age = 34;
+
+console.log("window this", this)
 learnFunction();
 function learnFunction() {
 
@@ -7,11 +9,34 @@ function learnFunction() {
         name: "Hamad",
         fatherName: "Muhammad Hamad",
         newfunctionl: function () {
-            return "thus s", this
+            return "thus s", this.name
 
         }
 
     }
+   
+
+    //sTRICK mODE
+    console.log("checkobject in Strick Mode", checkobject.newfunctionl())
+    
+
+//Non Strick Mode
+    
+    var nonstrickmode = checkobject.newfunctionl.bind(checkobject);
+    console.log("non Strick Mode ", nonstrickmode())
+
+    //When none strick mode with new object
+
+    var newobject = {
+
+
+        name: "areeba Noor"
+
+
+    }
+     var objectnewvar = checkobject.newfunctionl.bind(newobject)
+
+     console.log("Different Object ",objectnewvar())//get areeba noor not hamad
     if (canspeak && age > 45) {
         console.log("You are good Human")
 
